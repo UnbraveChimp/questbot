4663406178fa95dd8c60e644cb4e65d1caac3c2e
unbravechimp <ash@duckorg.com>
2026-05-22 15:44:10 +0200

    style: updates

--- apps/dash/app/routes/dashboard/dashboard.tsx
+++ apps/dash/app/routes/dashboard/dashboard.tsx
@@ -1,5 +1,6 @@
 import { Input } from '~/components/input/input';
 import { Button } from '~/components/button/button';
+import { Dropdown } from '~/components/dropdown/dropdown';
 export function meta() {
   return [
     { title: "Dashboard | QuestBot" },
@@ -9,7 +10,12 @@ export function meta() {
 
 export default function Auth() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-white">
+    <main className="flex min-h-screen items-center gap-4 justify-center bg-white">
+        <Dropdown width="250px">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+        </Dropdown>
         <Input width="250px" placeholder="wow" />
         <Button variant="1">1</Button>
         <Button variant="2">2</Button>
