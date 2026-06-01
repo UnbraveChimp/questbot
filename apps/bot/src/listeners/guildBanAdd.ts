a9f2ef9b61773883cac4569760da0083566b3c48
UnbraveChimp <unbravechimp@minerlegacy.net>
2026-06-02 00:23:22 +0200

    style(ban-listener): fix color

--- apps/bot/src/listeners/guildBanAdd.ts
+++ apps/bot/src/listeners/guildBanAdd.ts
@@ -1,5 +1,5 @@
 import { Listener } from '@sapphire/framework';
-import { AuditLogEvent, Colors, EmbedBuilder, Events, type GuildBan } from 'discord.js';
+import { AuditLogEvent, EmbedBuilder, Events, type GuildBan } from 'discord.js';
 import { getRecentAuditLogEntry, logEmbed } from '#lib/logging.js';
 
 export class GuildBanAddListener extends Listener<typeof Events.GuildBanAdd> {
@@ -15,7 +15,7 @@ export class GuildBanAddListener extends Listener<typeof Events.GuildBanAdd> {
 
 		const embed = new EmbedBuilder()
 			.setTitle('Member Banned')
-			.setColor(Colors.DarkRed)
+			.setColor(0xff6962)
 			.addFields(
 				{ name: 'Member', value: `${ban.user.tag} (${ban.user.id})`, inline: false },
 				{ name: 'Username', value: ban.user.toString(), inline: true },
