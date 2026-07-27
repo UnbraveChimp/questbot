2456821df1a44a973cc6ab8849abbda6a7ff1705
doodles172 <ash@duckorg.com>
2026-07-27 09:07:37 -0700

    feat: /user

--- apps/bot/src/commands/utility/profile/pfp.ts
+++ apps/bot/src/commands/utility/profile/pfp.ts
@@ -1,6 +1,6 @@
 import { Command } from '@sapphire/framework';
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, type SlashCommandUserOption } from 'discord.js';
-import { Colors } from '#utils/embeds.js';
+import type { SlashCommandUserOption } from 'discord.js';
+import { ASSET_SIZE, assetMessage } from '#utils/profile.js';
 
 export class PfpCommand extends Command {
 	public constructor(context: Command.LoaderContext, options: Command.Options) {
@@ -23,18 +23,8 @@ export class PfpCommand extends Command {
 
 		const user = interaction.options.getUser('user') ?? interaction.user;
 		// todo: in the future size could become an option rather than 4096 hard coded
-		const avatarUrl = user.displayAvatarURL({ size: 4096 });
+		const avatarUrl = user.displayAvatarURL({ size: ASSET_SIZE });
 
-		const embed = new EmbedBuilder()
-			.setColor(Colors.info)
-			.setTitle(`${user.displayName}`)
-			.setImage(avatarUrl)
-			.setFooter({ text: `ID: ${user.id}` });
-
-		const downloadRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
-			new ButtonBuilder().setLabel('Download').setStyle(ButtonStyle.Link).setURL(avatarUrl),
-		);
-
-		await interaction.editReply({ embeds: [embed], components: [downloadRow] });
+		await interaction.editReply(assetMessage(user, avatarUrl));
 	}
 }
